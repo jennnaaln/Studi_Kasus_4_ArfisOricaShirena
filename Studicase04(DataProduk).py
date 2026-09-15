0dc0be3d9f0283899fb0591797c38c59f3e790af
Arfis Orica <125890850+jennnaaln@users.noreply.github.com>
2026-09-15 21:14:47 +0800

    Update Studicase04(DataProduk).py

--- Studicase04(DataProduk).py
+++ Studicase04(DataProduk).py
@@ -1,9 +1,11 @@
+# dictionary data produk yang berisi key nama, harga, dan stok 
 data_produk = {
     "nama" : "beras",
     "harga" : "17000 per kg",
     "stok" : "70 kg"
 }
 
+# Menggunakan perulangan while untuk menampilkan beberapa menu pengelolaan data 
 while True:
     print("Menu Pengelolaan data produk")
     print("1. Tampilkan data produk")
@@ -14,25 +16,30 @@ while True:
 
     pilihan = input("Pilih menu(1-5): ")
 
+# jika user memilih menu 1 makan data produk ouput akan menampilkan data sekarang
     if pilihan == "1":
         print("Data Produk")
         print(data_produk)
 
+# Jika user memilih menu 2 maka akan menambah data baru yaitu data kategori ke dalam data produk
     elif pilihan == "2":
         data_produk["kategori"] = "sembako"
         print("Data kategori telah ditambahkan")
         print(data_produk)
 
+# Jika user memilih menu 3 maka akan mengganti harga dari 17000 menjadi 20000 di dalam data produk
     elif pilihan == "3":
         data_produk["harga"] = 20000
         print("Harga beras telah diganti")
         print(data_produk)
 
+# Jika user memilih menu 4 maka akan menghapus data kategori dalam data produk
     elif pilihan == "4":
         data_produk.pop("kategori")
         print("Data kategori telah dihapus")
         print(data_produk)
 
+# Jika user memilih menu 5 maka program akan selesai dan akan menampilkan data produk setelh perubahan
     else:
         pilihan == "5"
         print("Selesai")
